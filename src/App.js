7964a574d60d5c26b97870fc705880387408630e
Ganesh R <ganeshravichandran24@gmail.com>
2023-02-06 12:37:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import CustomerList from "./pages/Customers/CustomerList";
+import CustomerAdd from "./pages/Customers/CustomerAdd";
+import CustomerUpdate from "./pages/Customers/CustomerUpdate";
+import ProductList from "./pages/Products/ProductList";
+import ProductAdd from "./pages/Products/ProductAdd";
+import ProductUpdate from "./pages/Products/ProductUpdate";
+import OrderList from "./pages/Orders/OrderList";
+import OrderAdd from "./pages/Orders/OrderAdd";
+import OrderUpdate from "./pages/Orders/OrderUpdate";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/customerlist" element={<CustomerList />} />
+          <Route path="/customeradd" element={<CustomerAdd />} />
+          <Route path="/:id/customerupdate" element={<CustomerUpdate />} />
+          <Route path="/productlist" element={<ProductList />} />
+          <Route path="/productadd" element={<ProductAdd />} />
+          <Route path="/:id/productupdate" element={<ProductUpdate />} />
+          <Route path="/orderlist" element={<OrderList />} />
+          <Route path="/orderadd" element={<OrderAdd />} />
+          <Route path="/:id/orderupdate" element={<OrderUpdate />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
